163da6c0f0cd57843634cf90bfeab2b895daa577
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2021-08-30 21:40:10 +0700

    init ejs node

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render('pages/index');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
